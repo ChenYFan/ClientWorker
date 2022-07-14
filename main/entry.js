08f65c3756b877e08b05c11e7d136cabbda8d80e
ChenYFan <chen2778754364@foxmail.com>
2022-07-14 05:27:00 +0000

    U 发布2.2.1

--- main/entry.js
+++ main/entry.js
@@ -1,7 +1,14 @@
 import mainhandle from './handle/main.js'
+import pkgjson from '../package.json'
+import cons from './handle/cons.js'
 addEventListener('fetch', event => {
     event.respondWith(mainhandle(event.request))
 })
 addEventListener('install', function() {
+    cons.s(`ClientWorker@${pkgjson.version} installed!`)
     self.skipWaiting();
 });
+addEventListener('activate', function() {
+    cons.s(`ClientWorker@${pkgjson.version} activated!`)
+    self.clients.claim();
+})
